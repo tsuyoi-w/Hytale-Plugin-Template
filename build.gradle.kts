60386655c96f12c0372273550c9e6439fad0adcd
tsuyoi-w <lenny.wine@icloud.com>
2026-01-28 20:18:24 +0100

    Disable Javadoc tasks in build.gradle.kts
    Disable Javadoc generation for all tasks.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,10 @@ repositories {
     mavenCentral()
 }
 
+tasks.withType<Javadoc>().configureEach {
+    isEnabled = false
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
